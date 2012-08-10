78c4cb8dbddb8ccb61167d38d392fe6ef5449dd5
Andrey A Popov <apopov@vt.edu>
2012-08-10 14:23:53 -0400

    Still fixing DES, trying to get baseline for murmurhash3

---
+++ README.md
@@ -0,0 +1,9 @@
+Cryptod
+=======
+
+
+This is a simple Cryptography library written in D.
+
+It supports AES, and Threefish so far in terms of Ciphers.
+
+In terms of Hashes it supports SHA1 so far.
--- cryptod/src/hash/murmurhash3.d
+++ cryptod/src/hash/murmurhash3.d
@@ -32,18 +32,61 @@ module cryptod.hash.murmurhash3;
 	return (x << n) | (x >> (32 - n));
 }
 
+@safe pure uint fmix(uint h)
+{
+	h ^= h >> 16;
+	h *= 0x85ebca6b;
+	h ^= h >> 13;
+	h *= 0xc2b2ae35;
+	h ^= h >> 16;
+
+  return h;
+}
+//So far this is taken verbatim from murmurhash3.cpp
+//D specific niceness (Templates and all) will come later.
 
 uint murmurhash3_32(ubyte[] key, int len, uint seed)
 {
-	uint output;
+	const ubyte * data = cast(ubyte*)key;
 	const int nblocks = len / 4;
 
 	uint h1 = seed;
 	
 	uint c1 = 0xcc9e2d51;
 	uint c2 = 0x1b873593;
-
 	
+	const uint * blocks = cast(uint *)(data + nblocks*4);
 
-	return 1;
-}
+	for(int i = -nblocks; i; i++)
+	{
+		uint k1 = blocks[i];
+
+		k1 *= c1;
+		k1 = ROTL32(k1,15);
+		k1 *= c2;
+    
+		h1 ^= k1;
+		h1 = ROTL32(h1,13); 
+		h1 = h1*5+0xe6546b64;
+	}
+	
+	const ubyte * tail = cast(ubyte*)(data + nblocks*4);
+
+	uint k1 = 0;
+
+	switch(len & 3)
+	{
+		default:
+		case 3: k1 ^= tail[2] << 16;
+		case 2: k1 ^= tail[1] << 8;
+		case 1: k1 ^= tail[0];
+			k1 *= c1; k1 = ROTL32(k1,15); k1 *= c2; h1 ^= k1;
+	};
+
+
+	h1 ^= len;
+
+	h1 = fmix(h1);
+
+	return h1;
+} 
