e012eec6abeb73bf2797fb58697ae121ad710acc
Andrey A Popov <apopov@vt.edu>
2012-08-13 11:12:53 -0400

    Fixes

--- cryptod/src/tests/prngtests.d
+++ cryptod/src/tests/prngtests.d
@@ -113,14 +113,89 @@ class FrequencyTest : Test //This is both the monobits test and the block test.
 class RunsTest : Test
 {
 	private:
-	const numTimes = 0xfffff;
+	const numTimes = 0xffff;
 	const errorMargin = 0.001f;
 	PRNG p;
 	
-	double normalCDF(double d)
+	
+	/**
+	 * A normal CDF aproximation algorithm due to Hart
+	 */
+	double normalCDF(double z)
 	{
-		return 1;
+		double zabs;
+		double p;
+		double expntl,pdf;
+	
+		const double p0 = 220.2068679123761;
+		const double p1 = 221.2135961699311;
+		const double p2 = 112.0792914978709;
+		const double p3 = 33.91286607838300;
+		const double p4 = 6.373962203531650;
+		const double p5 = .7003830644436881;
+		const double p6 = .3526249659989109E-01;
+	
+		const double q0 = 440.4137358247522;
+		const double q1 = 793.8265125199484;
+		const double q2 = 637.3336333788311;
+		const double q3 = 296.5642487796737;
+		const double q4 = 86.78073220294608;
+		const double q5 = 16.06417757920695;
+		const double q6 = 1.755667163182642;
+		const double q7 = .8838834764831844E-1;
+	
+		const double cutoff = 7.071;
+		const double root2pi = 2.506628274631001;
+	
+		zabs = abs(z);
+	
+		if (z > 37.0) {
+	
+			p = 1.0;
+	
+			return p;
+	
+		}
+	
+		if (z < -37.0) {
+	
+			p = 0.0;
+	
+			return p;
+	
+		}
+	
+		expntl = exp(-.5f*zabs*zabs);
+	
+		pdf = expntl/root2pi;
+	
+	
+		if (zabs < cutoff) {
+	
+			p = expntl*((((((p6*zabs + p5)*zabs + p4)*zabs + p3)*zabs +
+			 p2)*zabs + p1)*zabs + p0)/(((((((q7*zabs + q6)*zabs +
+			 q5)*zabs + q4)*zabs + q3)*zabs + q2)*zabs + q1)*zabs +
+			 q0);
+	
+		} else {
+	
+			p = pdf/(zabs + 1.0/(zabs + 2.0/(zabs + 3.0/(zabs + 4.0/
+			 (zabs + 0.65)))));
+	
+		}
+	
+		if (z < 0.0) {
+	
+			return p;
+	
+		} else {
+	
+			p = 1.0 - p;
+	
+			return p;
+		}
 	}
+	
 	
 	public:
 	
@@ -173,14 +248,10 @@ class RunsTest : Test
 		
 		
 		//double mean = (2f*(cast(double)negativeRuns)*(cast(double)positiveRuns))/(cast(double)runs)+1f;
-		import std.stdio;
-		writeln(Z);
 		
-		double p = normalCDF(1-abs(Z));
+		double p = normalCDF(1f-abs(Z));
 		
-		writeln(p);
-		
-		return true;
+		return p>errorMargin;
 		//return mean < errorMargin;
 	}
 }
